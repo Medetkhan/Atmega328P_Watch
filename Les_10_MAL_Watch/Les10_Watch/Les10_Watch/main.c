8a55c6cebfb39c54af69fe9eab6089b4dcd03b7d
Medetkhan <92580071+Medetkhan@users.noreply.github.com>
2021-12-13 21:27:11 +0600

    Add files via upload
    Timer to 9 built on Timer Counter 1 of Atmega8 and 7 segment display(1bit).
    Used array of digits to pass counter_seconds variable as an argument for the array "digits[]". 
    In order to see simulation please use Proteus file in folder and hex firmware

---
+++ Les_10_MAL_Watch/Les10_Watch/Les10_Watch/main.c
@@ -0,0 +1,63 @@
+/*
+ * Les10_Watch.c
+ *
+ * Created: 12/13/2021 7:59:51 PM
+ * Author : User
+ */ 
+
+#include <avr/io.h>
+
+#define  SEG7_DDR DDRB
+#define  SEG7_PORT PORTB
+
+int counter_seconds = 0;
+
+
+
+int digits [10] =
+{
+	0b00111111, //0
+	0b00000110, //1
+	0b01011011, //2
+	0b01001111, //3
+	0b01100110, //4
+	0b01101101, //5
+	0b01111101, //6
+	0b00000111, //7
+	0b01111111, //8
+	0b01101111  //9
+};
+
+
+int main(void)
+{
+    //Prescaler 64 for 1TC
+    TCCR1B |=(1<<CS11)|(1<<CS10);
+    TCCR1B &=~(1<<CS12);
+    TCNT1 = 0;
+	
+	SEG7_DDR = 0xff;
+	SEG7_PORT =  0x00;
+	
+    while (1) 
+    {
+		if (TCNT1 == 15625 )// 1 sec
+		{
+			TCNT1 = 0;
+			
+				
+			SEG7_PORT = digits[counter_seconds];
+				
+			
+			counter_seconds++;
+			
+			if (counter_seconds == 10)
+			{
+				
+				counter_seconds = 0;
+				
+			}
+		}
+    }
+}
+
